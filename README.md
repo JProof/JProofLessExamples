04a1fd3ea14986874502c6fcbba1f026a190fedd
JProof <info@jproof.de>
2015-07-01 11:39:33 +0200

    Update variables

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ Less Compiler Library Examples for Joomla Developers and Template Designers
 
 Full Documentation and Download JProofLess under http://wiki.jproof.de/projects/joomla-library-jproof-less/wiki
 
+## Joomla Component/Modules/Plugins Developer
+
+Please submit mit your template skeleton to us. We will have a look to simply implement the JProofLess painless. 
+
 LessPHP developed by leafo
 http://leafo.net/lessphp/docs/
---
+++ basic_variables.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * PHP version 5.4
+ *
+ * @package    JProofLess Template Pattern
+ * @author     JProof(Romacron)  <info@jproof.de> http://www.jproof.de
+ * @copyright  2005 - 2015 jproof
+ * @license    WTFPL (../LICENSE)
+ * @link
+ * @see        http://wiki.jproof.de/projects/joomla-library-jproof-less
+ * @since
+ **/
+
+defined('_JEXEC') or die('Restricted access');
+
+$variablesArray  = array('colorstyle' => 'red', 'testVar' => 'pink', 'anVar' => '22px');
+$variablesObject = (object) $variablesArray;
+
+$multiDimensionsArray  = array($variablesArray, $variablesArray, "testVar" => 'green');
+$multiDimensionsObject = (object) $multiDimensionsArray;
+
+if(JLoader::import('jproofless.jproofless')){
+
+	$joomlaLess = JProofLess::getInstance();
+
+	$joomlaLess->setLessFile(JPATH_THEMES . '/' . $this->template . '/less/template.less');
+	$joomlaLess->setCssFile(JPATH_THEMES . '/' . $this->template . '/css/template.css');
+
+	// Chained Method
+	$fromAnOtherConfig = array('myTemplateConfigParameter' => '22px');
+	$fromAnConfig      = array('myTemplateConfigParameter' => '12px', 'myComponentVar' => '928px');
+
+	// sets @myTemplateConfigParameter:12px; @myComponentVar:928px
+	$joomlaLess->setVariables($fromAnConfig)->setVariables($fromAnOtherConfig);
+
+	// as Array
+	$joomlaLess->setVariables($variablesArray);
+
+	// or as Object
+	$joomlaLess->setVariables($variablesObject);
+
+	// or as multiDimensionsArray
+	$joomlaLess->setVariables($multiDimensionsArray);
+
+	// or as multiDimensionsObject
+	$joomlaLess->setVariables($multiDimensionsObject);
+
+	$joomlaLess->autoCompile();
+}
+else{
+	// adding an Log message if it is an good choice to install the JProofLess
+	JLog::add('JProofLess is missing: ' . __FILE__ . ' @see <a target="_blank" href="http://wiki.jproof.de/projects/joomla-library-jproof-less/wiki"><b>Wiki</b></a>',
+		  JLog::NOTICE);
+
+	// Adding each time the regular already rendered css into the template if the LessCompiler not found
+	JFactory::getDocument()->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/template.css');
+}
